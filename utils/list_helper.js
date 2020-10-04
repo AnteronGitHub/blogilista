5bf84a6ac2a20ab26c47cff54faecae8814f14a5
Antero Vainio <anterongugl@gmail.com>
2020-10-04 11:37:33 +0300

    4.4: Add totalLikes helper function and write test cases for it

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = blogs => {
   return 1
 }
 
+const totalLikes = blogs =>
+  blogs
+    .map(blog => blog.likes)
+    .reduce((l1, l2) => l1 + l2, 0)
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
