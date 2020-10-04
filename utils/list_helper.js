1de68881a95023b8bf69ec4a38575164a39474cf
Antero Vainio <anterongugl@gmail.com>
2020-10-04 11:47:20 +0300

    4.5: Add favoriteBlogs helper function and write test cases for it

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,7 +7,25 @@ const totalLikes = blogs =>
     .map(blog => blog.likes)
     .reduce((l1, l2) => l1 + l2, 0)
 
+const favoriteBlog = blogs => {
+  if (blogs.length === 0 ) {
+    return undefined
+  }
+  let favorite = blogs[0]
+  blogs.forEach(blog => {
+    if (blog.likes > favorite.likes) {
+      favorite = blog
+    }
+  })
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
