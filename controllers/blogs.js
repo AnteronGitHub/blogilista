2620f790e1175d727789da001ccef818ec8c7f59
Antero Vainio <anterongugl@gmail.com>
2020-10-10 14:01:20 +0300

    4.17: Link created blogs to users

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,13 +1,13 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-blogsRouter.get('/', (request, response, next) => {
-  Blog
+blogsRouter.get('/', async (request, response, next) => {
+  const blogs = await Blog
     .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-    .catch(error => next(error))
+    .populate('user', { username: 1, name: 1, id: 1 })
+
+  response.json(blogs.map(b => b.toJSON()))
 })
 
 blogsRouter.delete('/:id', (request, response, next) => {
@@ -38,15 +38,25 @@ blogsRouter.put('/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-blogsRouter.post('/', (request, response, next) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response, next) => {
+  const body = request.body
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => next(error))
+  const user = await User.findById(body.user)
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
+
+  const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog.toJSON())
 })
 
 module.exports = blogsRouter
--- controllers/users.js
+++ controllers/users.js
@@ -2,11 +2,12 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.get('/', (request, response, next) => {
-  User
+usersRouter.get('/', async (request, response, next) => {
+  const users = await User
     .find({})
-    .then(users => response.json(users))
-    .catch(error => next(error))
+    .populate('blogs', { url: 1, title: 1, author: 1, id: 1 })
+
+  response.json(users.map(u => u.toJSON()))
 })
 
 usersRouter.post('/', async (request, response, next) => {
